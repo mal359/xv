2ca287bc0c24031dfa8631c660227f19689d1e2b
Matthew A. Lukowicz <mlukowicz@sdf.org>
2024-02-20 00:36:21 -0500

    Add patch-xvtext.c from FreeBSD.

--- src/xvtext.c
+++ src/xvtext.c
@@ -181,7 +181,7 @@ static int  selectCodeset         PARM((TVINFO *));
 #endif
 #ifdef TV_MULTILINGUAL
 static void setCodingSpec   PARM((TVINFO *, struct coding_spec *));
-static void createCsWins    PARM((char *));
+static void createCsWins    PARM((const char *));
 static void openCsWin       PARM((TVINFO *));
 static void closeCsWin      PARM((TVINFO *));
 #endif
