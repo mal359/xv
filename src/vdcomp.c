18fc22932d136d13a81716bc8e4bd16ba04e213f
Matthew A. Lukowicz <mlukowicz@sdf.org>
2024-02-20 01:30:17 -0500

    Add patch-vdcomp.c from NetBSD pkgsrc.
    Don't use malloc.h  NOTE: Something that was cool in the early 90's isn't so cool in 2024.

--- src/vdcomp.c
+++ src/vdcomp.c
@@ -100,44 +100,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* All of this seems unecessary.  Does any reasonable system not
-have declarations for malloc and friends in stdlib.h? */
-#if 0
-/* include a malloc.h of some sort (if needed...most systems use stdlib.h) */
-#ifndef VMS   /* VMS hates multi-line "#if"s */
-   /*
-    * I want to use BSD macro for checking if this OS is *BSD or not,
-    * but the macro is defined in <sys/parm.h>, which I don't know all
-    * machine has or not.
-    */
-#  if !defined(ibm032)                    && \
-      !defined(__convex__)                && \
-      !(defined(vax) && !defined(ultrix)) && \
-      !defined(mips)                      && \
-      !defined(apollo)                    && \
-      !defined(pyr)                       && \
-      !defined(sequent)                   && \
-      !defined(__UMAXV__)                 && \
-      !defined(aux)                       && \
-      !defined(bsd43)                     && \
-      !defined(__bsd43)                   && \
-      !defined(__bsdi__)                  && \
-      !defined(__386BSD__)                && \
-      !defined(__FreeBSD__)               && \
-      !defined(__OpenBSD__)               && \
-      !defined(__NetBSD__)                && \
-      !defined(__DARWIN__)
-
-#    if defined(hp300) || defined(hp800) || defined(NeXT)
-#      include <sys/malloc.h>    /* it's in "sys" on HPs and NeXT */
-#    else
-#      include <malloc.h>        /* FIXME: should explicitly list systems that NEED this, not everyone that doesn't */
-#    endif
-
-#  endif /* !most modern systems */
-#endif /* !VMS */
-#endif
-
 #include <X11/Xos.h>
 
 #define TRUE         1
