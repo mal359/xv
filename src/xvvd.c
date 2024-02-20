3c9f6bf79d2df3e4c0862b0053011741c6e4041d
Matthew A. Lukowicz <mlukowicz@sdf.org>
2024-02-20 00:34:26 -0500

    Add patch-xvvd.c from FreeBSD.

--- src/xvvd.c
+++ src/xvvd.c
@@ -1072,6 +1072,8 @@ static void HUPhandler(XtPointer dummy, XtSignalId* Id)
 #endif
 }
 
+int InSignal = 0;
+
 static void vd_handler(int sig)
 {
     UsedSignal = sig;
@@ -1085,7 +1087,7 @@ static void INThandler(XtPointer dummy, XtSignalId* Id)
 #else
     sigblock(sigmask(UsedSignal));
 #endif
-
+    InSignal = 1;
     Quit(1); /*exit(1);*/
 }
 
